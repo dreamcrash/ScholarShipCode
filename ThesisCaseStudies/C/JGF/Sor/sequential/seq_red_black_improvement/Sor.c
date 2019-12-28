d2674e08aafe1ebaac39b8aab68fb54b4562545a
Bruno Medeiros <lesiano16@gmail.com>
2019-12-28 00:10:03 +0000

    SOR C Sequential red black version with reduction of conditional tests

---
+++ ThesisCaseStudies/C/JGF/Sor/sequential/seq_red_black_improvement/Sor.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+#include "Sor.h"
+
+void run (int size, int validation){
+    
+    const int datasizes[]={1000, 1500, 2000, 10000, 15000};
+    Sor *sor = malloc(sizeof(Sor));
+    sor->size = size;
+    sor->M = sor->N = datasizes[size];
+   
+    JGFinitialise   (sor);
+   
+    JGFKernel       (sor);
+    
+    if(validation)
+    {
+        JGFvalidate(sor->Gtotal, sor->size);
+    }
+
+    free(sor);
+
+}
+
+void JGFinitialise(Sor *sor){
+    
+    sor->JACOBI_NUM_ITER    = 100;
+    sor->RANDOM_SEED        = 10101010; 
+    sor->ops = 6 * sor->size * sor->size * sor->JACOBI_NUM_ITER;
+    
+    srand(sor->RANDOM_SEED);
+}
+
+
+void RandomMatrix(int M, int N, double G [M][N]){
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            G[i][j] = (((double)rand()/(double)RAND_MAX)) * 1e-6;
+        }     
+    }
+}
+
+void JGFKernel(Sor *sor){
+    
+    double (*G) [sor->N] = malloc(sizeof *G * sor->M);
+    RandomMatrix(sor->M, sor->N, G);
+
+    const double start  = omp_get_wtime();
+    sor_simulation (1.25, sor->M, sor->N, G, sor->JACOBI_NUM_ITER);
+    const double end    = omp_get_wtime();
+    printf("%f\n", (end - start));
+    
+    double Gtotal = 0;
+    
+    for (int i = 1; i < sor->M-1; i++) 
+    {
+        for (int j = 1; j < sor->N-1; j++) 
+        {
+            Gtotal += G[i][j];
+        }
+    }
+    sor->Gtotal = Gtotal;
+            
+    free(G);
+}
+
+void sor_simulation (double omega, int M, int N, 
+        double G[M][N], int num_iterations) {
+
+    double omega_over_four = omega * 0.25;
+    double one_minus_omega = 1.0 - omega;
+
+    int Mm1 = M-1;
+    int Nm1 = N-1;
+    int end = M-2;
+
+    for (int p=0; p< num_iterations; p++) 
+    {
+        row_iterations(1, end, omega_over_four, one_minus_omega, Mm1, Nm1, G);
+        row_iterations(2, end, omega_over_four, one_minus_omega, Mm1, Nm1, G);
+    }
+}
+
+/**
+ * In this version I managed to remove the conditions inside the inner loop
+ * for (int i = (p%2) + 1; i < Mm1+1; i+=2) making the code faster.
+ */
+
+void row_iterations(int begin, int end, double omega_over_four, double one_minus_omega, 
+                        int Mm1, int Nm1, double G[Mm1+1][Nm1+1]){
+    
+    int i = begin;
+    // Dealing with the first row
+    if(i == 1) 
+    { 
+        first_row(omega_over_four, one_minus_omega, Nm1, G[1], G[0], G[2]);
+        i += 2;
+    } 
+    
+    // Dealing with the middle rows
+    for (;i < end; i += 2) 
+    {
+        middle_rows(omega_over_four, one_minus_omega, Nm1, 
+                G[i], G[i-1], G[i+1], G[i-2]);
+    }
+    
+    // Dealing with the last iteration
+    if (i == Mm1) 
+    {
+        last_row(omega_over_four, one_minus_omega, Nm1,
+                G[i], G[i-1], G[i-2]);
+    } 
+    else 
+    {
+        middle_rows(omega_over_four, one_minus_omega, Nm1, 
+                G[i], G[i-1], G[i+1], G[i-2]);
+    }
+}
+
+void middle_rows(   const double omega_over_four, const double one_minus_omega, 
+                    const int Nm1, double *Gi, double *Gim1, 
+                    double *G1, double *G2) 
+{
+    const double *Gip1 = G1;
+    const double *Gim2 = G2;
+    int j;
+
+    int Nm3 = Nm1-2;
+    
+    // removing the if((j+1) != Nm1)  condition from the inside loop body
+    for (j = 1; j < Nm3; j += 2)
+    {
+        Gi[j] = omega_over_four * (Gim1[j] + Gip1[j] + Gi[j-1]
+                        + Gi[j+1]) + one_minus_omega * Gi[j];
+        Gim1[j+1] =omega_over_four * (Gim2[j+1] + Gi[j+1] + Gim1[j] + Gim1[j+2]) 
+                + one_minus_omega * Gim1[j+1];
+    }
+    Gi[j] = omega_over_four * (Gim1[j] + Gip1[j] + Gi[j-1] + Gi[j+1]) + 
+            one_minus_omega * Gi[j];
+			
+    if((j+1) != Nm1) 
+    {
+        Gim1[j+1]=omega_over_four * (Gim2[j+1] + Gi[j+1] + Gim1[j]
+                + Gim1[j+2]) + one_minus_omega * Gim1[j+1];
+    }
+}
+
+
+void last_row(  const double omega_over_four, const double one_minus_omega, 
+                const int Nm1, double *Gi, double *Gim1, double *Gim2_) 
+{
+                
+    const double *Gim2 = Gim2_;
+    // removing if((j+1) != Nm1) condition using Nm-1 instead of Nm1
+    const int Nm2 = Nm1 - 1;
+    for (int j = 1; j < Nm2; j += 2)
+    {
+        Gim1[j+1]=omega_over_four * (Gim2[j+1] + Gi[j+1] + Gim1[j]
+			+ Gim1[j+2]) + one_minus_omega * Gim1[j+1];
+    }
+}
+
+void first_row( const double omega_over_four, const double one_minus_omega, 
+                const int Nm1, double *Gi, double *Gim1, double *Gip1_) 
+{
+    const double *Gip1 = Gip1_;
+    
+    for (int j = 1; j < Nm1; j += 2)
+    {
+        Gi[j] = omega_over_four * (Gim1[j] + Gip1[j] + Gi[j-1]+ Gi[j+1]) 
+                + one_minus_omega * Gi[j];
+    }
+}
+
+int abs_            (int value)     {return (value > 0) ? value : -value;}
+double abs_double   (double value)  {return (value > 0) ? value : -value;}
+
+void JGFvalidate(double Gtotal, int size){
+   
+    double refval[] = {0.4980286235707718, 1.1200980290730285, 1.9980973961615818, 
+                        49.9861410004847784, 112.4579485559970067};
+    double dev = abs_double(Gtotal - refval[size]);
+    if (dev > 1.0e-12 )
+    {
+        printf("Validation failed\n");
+        printf("Gtotal = %.16f %.16f %d\n",Gtotal, dev ,size);
+    }
+}
