077521347cb43077e848240028393ffda5cbeba0
Bruno Medeiros <lesiano16@gmail.com>
2020-01-06 21:00:11 +0100

    Adding hybrid version: SM data redundacy OpenMP 4.0 and 4.5 features + DM version

---
+++ ThesisCaseStudies/C/MD/Hybrid/OpenMPSMDataRedundacy/Structs.h
@@ -0,0 +1,55 @@
+/* 
+ * File:   Structs.h
+ * Author: Dreamcrash
+ *
+ */
+
+#ifndef STRUCTS_H
+#define	STRUCTS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+// Using a SoA Layout
+typedef struct PARTICLES{
+
+    double *x,  *y,  *z;  // Positions
+    double *vx, *vy, *vz; // Velocities
+    double *fx, *fy, *fz; // Forces
+    int numberParticles;    
+    
+} Particles;
+
+typedef struct MD{
+    
+    Particles *particlesSOA;   // Use for the layouts SoA
+
+    int irep;
+    int istop;    
+    int iprint;
+    int mdsize;
+    int size;
+    int movemx;
+    int interactions;
+    double den;
+    double tref;
+    double h;
+    double epot, vir;
+    double rcoffs, side, hsq, hsq2, vel;
+    double sum, tscale, sc, ekin, ek;
+    
+    //DM CCCs
+    int processID;
+    int totalProcess;
+ 
+    //SM CCCs
+    int numThreads;
+}MD;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* STRUCTS_H */
+
---
+++ ThesisCaseStudies/C/MD/Hybrid/OpenMPSMDataRedundacy/main.c
@@ -0,0 +1,89 @@
+/** 
+ *  File:   main.c
+ *  Code Produced by Dreamcrash       
+ *                                                                         
+ *                   at                                       
+ *                                                                         
+ *  Uminho University. Using as base the code produced by    
+ *  Java Grande Benchmarking Project
+ * 
+ * Molecular Dynamic (MD) using: 
+ * -> Structure of Arrays (SoA) to represent the particles;
+ * -> SM;
+ *      -> Using data redundancy to deal with the race conditions;
+ *      -> Reducing that data using OpenMP 4.0 and OpenMP 4.5 features
+ *          -> Use a gcc version that supports those features (i.e, GCC 6.1+).
+ *          -> Note that for the largest inputs it might be necessary to tune the threads stack size:
+ *              -> With "ulimit -s <stacksize>" and OMP_STACKSIZE (e.g., 10m);
+ *      -> Improved load balancing by dividing the force calculation between particles among threads in a dynamic fashion.
+ * 
+ * -> Distribution Memory :
+ * 	-> Each Process will performance the entire MD simulation, with the
+ * force computation iterations being split across processes.
+ * 	-> After the end of the force computation: 
+ * 		-> the variables {fx, fy, fz} of the Particles;
+ * 		-> and the variables {epot, vir, interactions} of the MD Object;
+ *              will be reduced across all Processes (All_reduce).
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <omp.h>
+#include <mpi.h>
+#include "Structs.h"
+#include "main.h"
+#include "MDSoA.h"
+#include "functions_mpi.h"
+
+
+/** Data sizes:
+ *      8  -> 2048   particles;
+ *  	13 -> 8788   particles;
+ * 	17 -> 19652  particles;
+ *  	40 -> 256000 particles;
+ *  	50 -> 500000 particles;
+ * Arguments 
+ * 	argv[1] -> Validation (or not) of MD simulation
+ * 	argv[2] -> Simulation Size
+ * 	argv[3] -> # of Iterations
+ **/
+
+int main(int argc, char** argv){
+    
+    MPI_Init(&argc, &argv);
+    
+    int datasizes []    = {8,13,17,40,50};
+    int validation  	= (argc > 1) ? (strcmp(argv[1],"-v") == 0) :1;
+    int size 	  	= (argc > 2) ? atoi(argv[2]) : 0;
+    int iterations 	= (argc > 3) ? atoi(argv[3]) : 50;
+    int numThreads      = (argc > 4) ? atoi(argv[4]) : 2;
+    
+    MD *md = newMD(size, datasizes,iterations, numThreads);
+    
+    /** Running simulation */
+    MPI_Barrier(MPI_COMM_WORLD);
+    double start = omp_get_wtime();
+    runMD (md);
+    
+    MPI_Barrier(MPI_COMM_WORLD);
+    double end = omp_get_wtime();
+    
+    if(md->processID == 0) 
+    {
+        printf("Time:%f\n",end-start);
+        
+        if(validation)
+        {
+            validate(md);
+        }
+    }
+    
+    freeMD(md);
+  
+    MPI_Finalize();
+
+    return (EXIT_SUCCESS);
+}
+
